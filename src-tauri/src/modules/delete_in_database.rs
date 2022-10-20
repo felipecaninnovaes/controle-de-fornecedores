75898c452d22453300c3d2ca53f04ba8e4c7bfb3
FelipeCN <57776624+felipecaninnovaes@users.noreply.github.com>
2022-10-19 21:44:23 -0300

    Feature/listview (#2)
    Add list view in app

---
+++ src-tauri/src/modules/delete_in_database.rs
@@ -0,0 +1,13 @@
+pub mod delete_in_database {
+
+    pub use rusqlite::{params ,Connection, Result};
+    #[allow(dead_code)]
+    pub fn delete_in_database(
+        local: String,
+        id: String
+    ) -> Result<()> {
+        let conn = Connection::open(local)?;
+        conn.execute("delete from empresas WHERE id =(?1)", &[&id])?;
+        Ok(())
+    }
+}
---
+++ src/pages/ListPage.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react"
+import { select_from_database, delete_in_database } from "../modules/db"
+import { FiTrash2, FiRefreshCcw } from "react-icons/fi";
+import test from "node:test";
+
+interface Fornecedores {
+  id: String | null,
+  mes: String,
+  fornecedor: String,
+  dataPagamento: String,
+  valor: String,
+  banco: String,
+}
+
+const values = 2;
+export const Teste = () => {
+  
+}
+export const ListView = () => {
+  const [fornecedores, setFornecedores] = useState<Fornecedores[] | null[]>([]);
+  const [value, setvalue] = useState("");
+  useEffect(() => {
+    async function apiCall() {
+      const apiResponse: any = await select_from_database();
+      setFornecedores(apiResponse);
+    }
+    apiCall();
+  }, [value]);
+
+  const d = new Date();
+  let time = String(d.getTime());
+  return (
+    <table className="border-spacing-4 text-xl w-full rounded-md" onMouseEnter={() =>{setvalue(time)}} >
+      <thead>
+        <tr>
+          <th className="border-spacing-4">ID</th>
+          <th>MES REFRENCIA</th>
+          <th className="pl-2">DATA DE PAGAMENTO</th>
+          <th>FORNECEDOR</th>
+          <th>VALOR</th>
+          <th>BANCO</th>
+        </tr>
+      </thead>
+      {fornecedores.map(data => {
+        let idS: String = String(data?.id) as string
+        const d = new Date();
+        let times = String(d.getTime());
+        return (
+          <tbody key={String(data?.id)}>
+            <tr className=" border-solid border border-SC_border1"><td className="border  border-spacing-4">{data?.id}</td><td className="border">{data?.mes}</td><td className="border ">{data?.dataPagamento}</td><td className="border ">{data?.fornecedor}</td><td className="border ">{data?.valor}</td><td className="border">{data?.banco}</td>
+            <td className="border"><button className="" onClick={() =>{delete_in_database(idS); setvalue(times)}} ><FiTrash2 color="red"/></button></td>  
+            </tr>
+          </tbody>
+          
+        )
+      })}
+    </table>
+  );
+
+}
+
+export default ListView
