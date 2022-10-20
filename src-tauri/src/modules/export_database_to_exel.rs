0fb63bd6fec6ccb317b5128a4ceaf7d51f060b07
FelipeCN <felipecaninn14@gmail.com>
2022-10-20 10:14:47 -0300

    changed export schema

--- src-tauri/src/modules/export_database_to_exel.rs
+++ src-tauri/src/modules/export_database_to_exel.rs
@@ -3,6 +3,7 @@ pub mod export_database_to_exel {
     use rust_xlsxwriter::Workbook;
 
     pub fn export_database_to_exel(local: String, mes_value: String, xlsx_folder: String) -> Result<()> {
+        #[allow(dead_code)]
         struct Empresas {
             id: i32,
             mes: String,
@@ -15,22 +16,16 @@ pub mod export_database_to_exel {
         let mut workbook = Workbook::new(&xlsx_folder);
         let worksheet = workbook.add_worksheet();
         worksheet
-            .write_string_only(0, 0, "Lancamento")
+            .write_string_only(0, 0, "Data de Pagamento")
             .expect("Falha ao gravar");
         worksheet
-            .write_string_only(0, 1, "Mes")
+            .write_string_only(0, 1, "Fornecedor")
             .expect("Falha ao gravar");
         worksheet
-            .write_string_only(0, 2, "Fornecedor")
+            .write_string_only(0, 2, "valor")
             .expect("Falha ao gravar");
         worksheet
-            .write_string_only(0, 3, "Data de Pagamento")
-            .expect("Falha ao gravar");
-        worksheet
-            .write_string_only(0, 4, "valor")
-            .expect("Falha ao gravar");
-        worksheet
-            .write_string_only(0, 5, "banco")
+            .write_string_only(0, 3, "banco")
             .expect("Falha ao gravar");
 
         let mut stmt = conn.prepare("SELECT id, mes, fornecedor, dataPagamento, valor, banco FROM empresas WHERE mes = :mes")?;
@@ -47,22 +42,9 @@ pub mod export_database_to_exel {
         let mut num: u32 = 1;
         for empresas in empresas_iter {
             worksheet
-                .write_string_only(num, 0, empresas.as_ref().unwrap().id.to_string().as_str())
-                .expect("Falha ao gravar");
-            worksheet
-                .write_string_only(num, 1, empresas.as_ref().unwrap().mes.to_string().as_str())
-                .expect("Falha ao gravar");
-            worksheet
                 .write_string_only(
                     num,
-                    2,
-                    empresas.as_ref().unwrap().fornecedor.to_string().as_str(),
-                )
-                .expect("Falha ao gravar");
-            worksheet
-                .write_string_only(
-                    num,
-                    3,
+                    0,
                     empresas
                         .as_ref()
                         .unwrap()
@@ -74,14 +56,21 @@ pub mod export_database_to_exel {
             worksheet
                 .write_string_only(
                     num,
-                    4,
+                    1,
+                    empresas.as_ref().unwrap().fornecedor.to_string().as_str(),
+                )
+                .expect("Falha ao gravar");
+            worksheet
+                .write_string_only(
+                    num,
+                    2,
                     empresas.as_ref().unwrap().valor.to_string().as_str(),
                 )
                 .expect("Falha ao gravar");
             worksheet
                 .write_string_only(
                     num,
-                    5,
+                    3,
                     empresas.as_ref().unwrap().banco.to_string().as_str(),
                 )
                 .expect("Falha ao gravar");
