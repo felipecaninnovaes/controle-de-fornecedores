af8c6f58d9f89a4ac641fe647251262ff2b2b5af
FelipeCN <felipecaninn14@gmail.com>
2022-11-14 21:46:46 -0300

    Fix: edit service not working

--- src/shared/services/fornecedores-services/insert.ts
+++ src/shared/services/fornecedores-services/insert.ts
@@ -8,7 +8,5 @@ export async function insert_db(fornecedor: string, cnpj: string, data_pagamento
     const mesSplitValues = data.split('/')
     const MonthAndYear = String(mesSplitValues[0] + '/' + mesSplitValues[1])
     data = String(mesSplitValues[2] + '/' + mesSplitValues[1] + '/' + mesSplitValues[0])
-    if (fornecedor === '' || valor === '' || banco === '' || data === '' || multa === '' || juros === '') {
-        return (await invoke('insert_database_fn', { local: appDirPath, mes: MonthAndYear, fornecedor: fornecedor, cnpj: cnpj, dataPagamento: data, valor: valor, multa: multa, juros: juros, banco: banco }))
-    }
+    return (await invoke('insert_database_fn', { local: appDirPath, mes: MonthAndYear, fornecedor: fornecedor, cnpj: cnpj, dataPagamento: data, valor: valor, multa: multa, juros: juros, banco: banco }))
 }
