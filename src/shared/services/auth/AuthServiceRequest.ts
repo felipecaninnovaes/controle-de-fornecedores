9110b268cb57ad43160accd80d3d4092bf494aa8
FelipeCN <felipecaninn14@gmail.com>
2023-11-30 20:39:57 -0300

    update deps

--- src/shared/services/auth/AuthServiceRequest.ts
+++ src/shared/services/auth/AuthServiceRequest.ts
@@ -10,9 +10,9 @@ interface IToken {
 export async function verify_username_and_passowrd(username: string, password: string) {
   const appDirPath = await appConfigDir() + 'database.sqlite'
   const result: Array<IToken> = (await invoke('verify_user_password_fn', { local: appDirPath, username: username, password: password }))
-  
+
   if (!result.length) {
     return ('')
   }
-  return ('{"accessToken": ' + '"' + result[0].key +'",'+ '"userID": ' + '"' + result[0].id +'"' +'}')
+  return ('{"accessToken": ' + '"' + result[0].key + '",' + '"userID": ' + '"' + result[0].id + '"' + '}')
 }
--- src/shared/services/auth/TokenValidation.ts
+++ src/shared/services/auth/TokenValidation.ts
@@ -3,8 +3,7 @@ import { appConfigDir } from '@tauri-apps/api/path'
 
 
 export async function tokenValidation(id: string, token: string) {
-const appDirPath = await appConfigDir() + 'database.sqlite'
-const validate: boolean  = await invoke('check_token_valid_fn', { local: appDirPath, token: token, id: id })
-console.log(validate)
-return(validate)
+    const appDirPath = await appConfigDir() + 'database.sqlite'
+    const validate: boolean = await invoke('check_token_valid_fn', { local: appDirPath, token: token, id: id })
+    return (validate)
 }
